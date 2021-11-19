af747ec712f11779cb6246af431d74c3425213b6
otverskoj <61247849+otverskoj@users.noreply.github.com>
2021-11-19 17:47:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Another print!')
+    print('WE NEED MORE PRINTS!!!')
 
 
 if __name__ == '__main__':
