f09a5e21687f58196bcfe3bbf00e65289b08779b
Oleg Tverskoy <otverskoj@gmail.com>
2021-12-07 17:18:59 +0600

    Change main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def my_superfunction():
+def my_super_function():
     print('What an awesome print!')
 
 
@@ -7,7 +7,7 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
-    my_superfunction()
+    my_super_function()
 
 
 if __name__ == '__main__':
